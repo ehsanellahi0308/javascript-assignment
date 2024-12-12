3bc9ae6a12ff1d282bd6b98a7bcfb465ae86b1ff
ehsanellahi0308 <ehsanvicky0@gmail.com>
2024-12-12 10:42:38 +0500

    Light Bulb Experiment 1

---
+++ fast.js
@@ -0,0 +1,8 @@
+var userInput = prompt("Enter 1 to turn on the light, or 0 to turn it off:");
+if (userInput === "1") {
+    console.log("The light is ON");
+} else if (userInput === "0") {
+    console.log("The light is OFF");
+} else {
+    console.log("Invalid Input");
+}
