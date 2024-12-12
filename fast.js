1d853cc1c72adf6ddcc1c2353cee8a469ff074a3
ehsanellahi0308 <ehsanvicky0@gmail.com>
2024-12-12 10:59:01 +0500

    eligible to vote

--- fast.js
+++ fast.js
@@ -1,8 +1,7 @@
-var userInput = prompt("Enter 1 to turn on the light, or 0 to turn it off:");
-if (userInput === "1") {
-    console.log("The light is ON");
-} else if (userInput === "0") {
-    console.log("The light is OFF");
-} else {
-    console.log("Invalid Input");
+let userinput = prompt( " 18 "); {
+    if (age >= 18) {
+        console.log("You are eligible to vote.");
+    } else {
+        console.log("You are not eligible to vote.");
+    }
 }
